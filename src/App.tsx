b5ffc1d52a8e80cef0cb7fa54b605ca8f5bb6a08
DevOnTheGo <brian2504156@hybridge.education>
2025-10-22 21:07:57 -0600

    chore: initialize project with React, TypeScript and Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">
+        ¡Hola, este es mi portafolio con React + Tailwind + TS!
+      </h1>
+    </div>
+  );
+}
+
+export default App;

1395315a42d13c3e6c764b66faff7883c4469e5a
DevOnTheGo <brian2504156@hybridge.education>
2025-10-24 23:42:31 -0600

    chore: initialize skills section with basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Hero from "./components/Hero.tsx";
 import Header from "./components/Header.tsx";
 import About from "./components/About.tsx";
+import Skills from "./components/Skills.tsx";
 function App() {
   return (
     <>
       <Header />
       <Hero />
       <About />
+      <Skills />
     </>
   );
 }
